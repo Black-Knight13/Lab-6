dc248c6f1fb639b0c5ebc32e874ed59df6d23e2f
Black-Knight13 <127360994+Black-Knight13@users.noreply.github.com>
2023-03-09 14:31:34 -0500

    Add files via upload

---
+++ git.py
@@ -0,0 +1,35 @@
+def encode_password(password):
+    alpha = ""
+    for i in password:
+        alpha += str(int(i) + 3)
+
+    return alpha
+
+def decode_password(d_password):
+    theta = ""
+    for i in d_password:
+        theta += str(int(i) - 3)
+    return theta
+
+
+
+
+print(encode_password("12345555"))
+print((decode_password("4567888")))
+
+
+def main():
+    password = input("Please input your password: ")
+    while True:
+        print(f"Here is your encoded password: {encode_password(password)}")
+
+        password = input("Please input your password: ")
+        print(f"Here is your decoded password: {decode_password(password)}")
+        break
+
+
+
+
+
+if __name__ == "__main__":
+    main()
