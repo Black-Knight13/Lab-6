d5e3c03ed289b09e5b333a280d17f068cf246fa2
Jackson Turnbull <jturnbull@ufl.edu>
2023-03-15 21:57:21 -0400

    fixed encoder

--- git.py
+++ git.py
@@ -1,27 +1,45 @@
+# encoder written by Jackson Turnbull
+
 def encode_password(password):
     alpha = ""
-    for i in password:
-        alpha += str(int(i) + 3)
+    theta = password
+    for i in theta:
+        if i in range(1, 7):
+            theta = str(int(i) + 3)
+            alpha += theta
+
+        if int(i) >= 7:
+            if i == '7':
+                theta = '0'
+                alpha += theta
+            elif i == '8':
+                theta = '1'
+                alpha += theta
+            elif i == '9':
+                theta = '2'
+                alpha += theta
 
     return alpha
 
-def decode_password(d_password):
-    theta = ""
-    for i in d_password:
-        theta += str(int(i) - 3)
-    return theta
-
-
+# Test case #1 print(encode_password("999999")) returns 222222
 
 
 def main():
-    password = input("Please input your password: ")
+    print("Menu \n------------- \n1.Encode \n2.Decode \n3.Quit")
     while True:
-        print(f"Here is your encoded password: {encode_password(password)}")
+        option = int(input("Please enter an option: "))
 
-        password = input("Please input your password: ")
-        print(f"Here is your decoded password: {decode_password(password)}")
-        break
+        if option == 1:
+            theta = input("Please enter your password to encode: ")
+            alpha = encode_password(theta)
+            print("Your password has been encoded and stored!")
+
+        elif option == 3:
+            break
+
+
+if __name__ == "__main__":
+    main()
 
 
 
