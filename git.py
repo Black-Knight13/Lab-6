a843bfc810a1cd32579afc97c6961631052971e0
s1vart <shepherdt@ufl.edu>
2023-03-17 20:53:09 -0400

    fixed broken encoder and added decoder along with main logic - travis

--- git.py
+++ git.py
@@ -4,7 +4,7 @@ def encode_password(password):
     alpha = ""
     theta = password
     for i in theta:
-        if int(i) in range(1, 7):
+        if int(i) in range(0, 7):
             theta = str(int(i) + 3)
             alpha += theta
 
@@ -21,19 +21,40 @@ def encode_password(password):
 
     return alpha
 
+
+def decode_password(password):
+    decoded = ''
+    a = 0
+    for i in password:
+        if int(i) in range(3,10):
+            a = int(i) - 3
+            decoded += str(a)
+        elif int(i) == 2:
+            a = 9
+            decoded += str(a)
+        elif int(i) == 1:
+            a = 8
+            decoded += str(a)
+        elif int(i) == 0:
+            a = 7
+            decoded += str(a)
+    return decoded
+
 print(encode_password("12345666"))
 
 
 def main():
     print("Menu \n------------- \n1.Encode \n2.Decode \n3.Quit")
+    alpha = ''
     while True:
         option = int(input("Please enter an option: "))
 
         if option == 1:
-            theta = input("Please enter your password to encode: ")
+            theta = str(input("Please enter your password to encode: "))
             alpha = encode_password(theta)
             print("Your password has been encoded and stored!")
-
+        elif option == 2:
+            print(f'The encoded password is {alpha}, and the original password is {decode_password(alpha)}.')
         elif option == 3:
             break
 
